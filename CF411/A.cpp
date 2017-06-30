85d6a0ee5f4b8462d854ade2e671c3462ba57b78
Zhe Zheng <zhez@supermicro.com>
2017-06-29 18:48:50 -0700

    A simple answer

--- CF411/A.cpp
+++ CF411/A.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
+#define ll long long
+
 using namespace std;
 
 int main(){
-	int a, b;
-	cin >> a >> b;
-	int res=0, temp=0, i=2;
-	while(i<=a){
-		int count =0;
-		if(i>b) break;
-		for(int j=a; j<=b; j++){
-			if(j%i==0) count++;
-		}
-		if(count>=temp){
-			temp = count;
-			res = i;
-		}
-		i++;
-	}
-	cout << res << endl;
+	ll a, b;
+	cin  >> a >> b;
+	if(a==b) cout << a << endl;
+	else cout << "2" << endl;
 	return 1;
 }
