f2c534ee7a24313818175f058248ae257010b87e
Zhe Zheng <zhez@supermicro.com>
2017-06-29 17:30:18 -0700

    cf410 B

---
+++ CF410/A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string s;
+	cin >> s;
+	int l=0, r=s.size()-1, count=0;
+	while(l<r){
+		if(s.at(l)!=s.at(r)){
+			if(count>0){
+				cout << "NO" << endl;
+				return 0;
+			}
+			count++;
+		}
+		l++; r--;
+	}
+
+	if(count==1 || l==r) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+
+	
+}
